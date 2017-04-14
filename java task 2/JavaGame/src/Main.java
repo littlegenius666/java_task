6d61c0c47e9c122eb1fb6893f7183d62142cd7e4
littlegenius666 <littlegenius666@gmail.com>
2017-04-14 19:04:27 +0300

    Added 2nd task (without tests)

---
+++ java task 2/JavaGame/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Yelyzaveta_Horbachen on 13.04.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Integer low=0;
+        Integer high=100;
+        Model model = new Model(low,high);
+        View view = new View();
+        Controller game = new Controller(model, view);
+        game.start();
+    }
+}
